33424613b365c2632f2b65a86131054679cb9b57
honnage <hon_da-1998@hotmail.com>
2022-06-12 15:39:33 +0700

    Using Gradients font_awesome

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mystock/src/config/theme.dart' as custom_theme;
 import 'package:mystock/src/pages/login/widgets/header.dart';
 
@@ -17,12 +18,20 @@ class LoginPage extends StatelessWidget {
             ),
           ),
           Column(
-            children: const [
-              Header(),
-              Text('form'),
-              Text('fogot password'),
-              Text('SSO'),
-              Text('register'),
+            children: [
+              const Header(),
+              IconButton(
+                  icon: const FaIcon(
+                    FontAwesomeIcons.camera,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    // todo
+                  }),
+              const Text('form'),
+              const Text('fogot password'),
+              const Text('SSO'),
+              const Text('register'),
             ],
           ),
         ],
