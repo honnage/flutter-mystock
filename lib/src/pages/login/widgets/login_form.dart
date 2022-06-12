f76ac08e5aa054f1595a0555aa8cba3e33eac368
honnage <hon_da-1998@hotmail.com>
2022-06-12 15:48:32 +0700

    fix login form

--- lib/src/pages/login/widgets/login_form.dart
+++ lib/src/pages/login/widgets/login_form.dart
@@ -1,18 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mystock/src/config/theme.dart' as custom_theme;
 
 class LoginForm extends StatelessWidget {
   const LoginForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      child: FormInput(),
+    return Stack(
+      children: [
+        _builForm(),
+        _builSubmitFormButton(),
+      ],
     );
+  }
+
+  Card _builForm() {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 22, left: 22, right: 22),
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(22.0),
+        child: FormInput(),
+      ),
+    );
+  }
+
+  Container _builSubmitFormButton() =>
+      Container(
+        width: 220,
+        height: 50,
+        decoration: _boxDecoration(),
+      );
+
+  BoxDecoration _boxDecoration() {
+    const gradientStart = custom_theme.Theme.gradientStart;
+    const gradientEnd = custom_theme.Theme.gradientEnd;
+
+    // ignore: prefer_function_declarations_over_variables
+    final boxShadowItem = (Color color) =>
+        BoxShadow(
+          color: color,
+          offset: Offset(1.0, 6.0),
+          blurRadius: 20.0,
+        );
+
+    return BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+        boxShadow: [
+          boxShadowItem(gradientStart),
+          boxShadowItem(gradientEnd),
+        ],
+        gradient: const LinearGradient(
+          colors: [
+            gradientEnd,
+            gradientStart,
+          ],
+          begin: FractionalOffset(0.0, 0.0),
+          end: FractionalOffset(1.0, 1.0),
+          stops: [0.0, 1.0],
+        ));
   }
 }
 
+
+Card _builForm() {
+  return Card(
+    margin: const EdgeInsets.only(bottom: 22, left: 22, right: 22),
+    elevation: 2.0,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8.0),
+    ),
+    child: const Padding(
+      padding: EdgeInsets.all(22.0),
+      child: FormInput(),
+    ),
+  );
+}
+
 class FormInput extends StatelessWidget {
+  final _color = Colors.black54;
+
   const FormInput({
     Key? key,
   }) : super(key: key);
@@ -20,30 +91,53 @@ class FormInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        TextField(
-          decoration:  InputDecoration(
-              border: InputBorder.none,
-              labelText: 'Email Address',
-              hintText: 'test@mail.com',
-              icon: FaIcon(
-                FontAwesomeIcons.envelope,
-                size: 22.0,
-              )
-          ),
+      children: [
+        _buildUsername(),
+        const Divider(
+          height: 22,
+          thickness: 1,
+          indent: 22,
+          endIndent: 13,
         ),
-        TextField(
-          decoration:  InputDecoration(
-              border: InputBorder.none,
-              labelText: 'Password',
-              icon: FaIcon(
-                FontAwesomeIcons.lock,
-                size: 22.0,
-              )
-          ),
-          obscureText: true,
-        ),
+        _buildPassword(),
       ],
     );
   }
+
+  TextStyle _textStyle() =>
+      TextStyle(
+        fontWeight: FontWeight.w500,
+        color: _color,
+      );
+
+  TextField _buildPassword() =>
+      TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          labelText: 'Password',
+          labelStyle: _textStyle(),
+          icon: FaIcon(
+            FontAwesomeIcons.lock,
+            size: 22.0,
+            color: _color,
+          ),
+        ),
+        obscureText: true,
+      );
+
+  TextField _buildUsername() =>
+      TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          labelText: 'Email Address',
+          // hintText: 'test',
+          labelStyle: _textStyle(),
+          icon: FaIcon(
+            FontAwesomeIcons.envelope,
+            size: 22.0,
+            color: _color,
+          ),
+        ),
+        obscureText: false,
+      );
 }
