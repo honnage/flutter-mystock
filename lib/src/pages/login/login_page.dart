9d7abd8c586d96ca7fa0aa4c5f985dbad4dff534
honnage <hon_da-1998@hotmail.com>
2022-06-13 15:07:16 +0700

    Design a login form

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mystock/src/config/theme.dart' as custom_theme;
-import 'package:mystock/src/constants/asset.dart';
 import 'package:mystock/src/pages/login/widgets/header.dart';
+import 'package:mystock/src/pages/login/widgets/login_form.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -19,20 +19,13 @@ class LoginPage extends StatelessWidget {
             ),
           ),
           Column(
-            children:  [
-              const Header(),
-              IconButton(
-                  icon: const FaIcon(
-                    FontAwesomeIcons.camera,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // todo
-                  }),
-              const Text('form'),
-              const Text('forgot password'),
-              const Text('SSO'),
-              const Text('register'),
+            children:  const [
+              Header(),
+              LoginFrom(),
+              Text('form'),
+              Text('forgot password'),
+              Text('SSO'),
+              Text('register'),
             ],
           ),
         ],
